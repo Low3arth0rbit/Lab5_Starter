bb66ce2cadf428898554ea422f35efda1fa9e644
Low3arth0rbit <hungryleo9316@gmail.com>
2023-05-02 00:45:07 -0700

    test for voice select

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,27 @@
 // explore.js
 
+
+
 window.addEventListener('DOMContentLoaded', init);
 
+
 function init() {
-  // TODO
+  const synth = window.speechSynthesis;
+  let voices = [];
+  const voiceSelect = document.getElementById("voice-select")
+  voices = synth.getVoices();
+  console.log(speechSynthesis.getVoices());
+  for (let i = 0; i < voices.length; i++) {
+    const option = document.createElement("option");
+    option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+    if (voices[i].default) {
+      option.textContent += " — DEFAULT";
+    }
+
+    option.setAttribute("data-lang", voices[i].lang);
+    option.setAttribute("data-name", voices[i].name);
+    voiceSelect.add(option);
+  }
+  console.log(voices);
 }
