47369f2462f3e0fb5a356bbd0efca08c2e788b16
Low3arth0rbit <hungryleo9316@gmail.com>
2023-05-01 23:44:04 -0700

    added horn image/audio selector and basic play button

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -2,6 +2,24 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
+
 function init() {
-  // TODO
+  //horn image & audio selector
+  const horn = document.getElementById("horn-select");
+  const img = document.querySelector("img");
+  const aud = document.querySelector("audio");
+  const volume = aud.volume;
+  horn.addEventListener("change", (event) => {
+    img.src = "./assets/images/" + horn.value + ".svg";
+    aud.src = "./assets/audio/" + horn.value + ".mp3";
+    console.log(img.src);
+    console.log(aud.src);
+    console.log(volume);
+  });
+
+  //volume slider
+
+  //play horn audio
+  const playBtn = document.querySelector("button");
+  playBtn.addEventListener("click", (event) => {aud.play();});
 }
