573c96c681df0865e5148b232b141779bc3527ac
Low3arth0rbit <hungryleo9316@gmail.com>
2023-05-02 01:39:05 -0700

    added speech synthesis

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,27 +1,35 @@
 // explore.js
 
-
-
 window.addEventListener('DOMContentLoaded', init);
-
 
 function init() {
   const synth = window.speechSynthesis;
   let voices = [];
-  const voiceSelect = document.getElementById("voice-select")
-  voices = synth.getVoices();
-  console.log(speechSynthesis.getVoices());
-  for (let i = 0; i < voices.length; i++) {
-    const option = document.createElement("option");
-    option.textContent = `${voices[i].name} (${voices[i].lang})`;
-
-    if (voices[i].default) {
-      option.textContent += " — DEFAULT";
+  const voiceSelect = document.getElementById("voice-select");
+  synth.addEventListener("voiceschanged", (event) => {
+    voices = window.speechSynthesis.getVoices();
+    console.log(voices);
+    for (let i = 0; i < voices.length; i++) {
+      const option = document.createElement("option");
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+  
+      option.setAttribute("data-lang", voices[i].lang);
+      option.setAttribute("data-name", voices[i].name);
+      voiceSelect.add(option);
     }
+  });
 
-    option.setAttribute("data-lang", voices[i].lang);
-    option.setAttribute("data-name", voices[i].name);
-    voiceSelect.add(option);
-  }
-  console.log(voices);
+  const talk = document.querySelector("button");
+  talk.addEventListener("click", (event) => {
+    document.querySelector("img").src = "./assets/images/smiling-open.png";
+    const text = new SpeechSynthesisUtterance(document.getElementById("text-to-speak").value);
+    const selected = voiceSelect.selectedOptions[0].getAttribute("data-name");
+    for (let i = 0; i < voices.length; i++) {
+      if (voices[i].name === selected) {
+        text.voice = voices[i];
+      }
+    }
+    synth.speak(text);
+    text.addEventListener("end", (event) => {document.querySelector("img").src = "./assets/images/smiling.png";});
+  })
 }
